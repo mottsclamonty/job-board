ac4d4653f5b77c6d5db3b731a4712a8a4641c2e5
Connor Lamont <connor@connorlamont.dev>
2023-06-28 15:18:50 -0400

    Error classes and handling

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+import BadRequestError from './bad-request.js';
+import NotFoundError from './not-found.js';
+import UnauthorizedError from './unauthorized.js';
+
+export { BadRequestError, NotFoundError, UnauthorizedError };
