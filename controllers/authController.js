3525c33cd83a1675cf95ffe754e311229a93babf
Connor Lamont <connor@connorlamont.dev>
2023-06-28 16:02:22 -0400

    password hashing & jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, UnauthorizedError } from '../errors/index.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -14,6 +16,9 @@ const register = async (req, res) => {
   }
   const user = await User.create({ name, email, password });
 
+  // jwt token containing userID
+  const token = user.createJWT();
+
   res.status(StatusCodes.CREATED).json({
     user: {
       email: user.email,
@@ -23,11 +28,24 @@ const register = async (req, res) => {
     },
 
     location: user.location,
+    token,
   });
 };
 
 const login = async (req, res) => {
-  res.send('login');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+  const user = await User.findOne({ email });
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    throw new UnauthorizedError('Email and password do not match');
+  }
+
+  res.status(200).json({ message: 'Login successful' });
 };
 
 const updateUser = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide name'],
@@ -22,6 +24,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide password'],
     minLength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -37,4 +40,17 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-export default mongoose.model('User', userSchema);
+// Sign and return a jwt with userID as payload
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPW = await bcrypt.hash(this.password, salt);
+  this.password = hashedPW;
+});
+
+export default mongoose.model('User', UserSchema);
