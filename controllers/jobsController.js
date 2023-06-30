b5ca53dd4b8a11f716b8dc3d2f84edca236cf8d2
Connor Lamont <connor@connorlamont.dev>
2023-06-30 18:03:28 -0400

    Job model and createJob controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,21 @@
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../errors/index.js';
+import Job from '../models/Job.js';
+import User from '../models/User.js';
 const createJob = async (req, res) => {
-  res.send('creating job');
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  req.body.createdBy = req.user.userId;
+
+  const newJob = await Job.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({
+    job: newJob,
+  });
 };
 
 const deleteJob = async (req, res) => {
